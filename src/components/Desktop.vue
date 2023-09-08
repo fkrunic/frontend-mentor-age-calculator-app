56a287504bf19334d905fe98018fa68eaa408927
Filip Krunic <fkrunic9@gmail.com>
2023-09-07 21:34:48 -0700

    desktop design

--- src/components/Desktop.vue
+++ src/components/Desktop.vue
@@ -6,14 +6,37 @@ import { content } from '../content';
   <div class="flex flex-col items-center justify-center min-h-screen">
 
     <!-- Card -->
-    <div class="flex flex-col gap-4 bg-white">
+    <div class="
+      flex 
+      flex-col 
+      gap-12
+
+      p-14
+      w-[52rem]  
+
+      rounded-[20px]
+      rounded-br-[200px]
+      
+      bg-white
+      ">
 
       <!-- Date Entry -->
-      <div class="flex flex-row">
-        <div class="flex flex-col" v-for="entry of content.inputs">
-          <p>{{ entry.title }}</p>
+      <div class="flex flex-row gap-8">
+        <div class="flex flex-col gap-2" v-for="entry of content.inputs">
+          <p class="text-sm tracking-[0.2rem] text-smokey-gray/80">{{ entry.title }}</p>
           <input 
-            class="outline-none w-40 p-4 rounded border-solid border-[1px] border-light-gray"
+            class="
+              w-40 
+              p-4
+
+              text-3xl
+              outline-none  
+              
+              rounded-lg
+              border-solid 
+              border-[1px] 
+              border-light-gray
+              "
             type="text" 
             :placeholder="entry.placeholder">
         </div>
@@ -23,15 +46,15 @@ import { content } from '../content';
       <div class="relative border-solid border-[1px] border-light-gray">
 
         <!-- Icon -->
-        <div class="absolute p-8 bg-purple rounded-full">
+        <div class="absolute p-6 right-0 -top-12 bg-purple rounded-full">
           <img class="w-12 h-12" src="../assets/icon-arrow.svg" alt="bla">
         </div>        
       </div>
 
       <!-- Display -->
-      <div class="flex flex-col">
-        <div class="flex flex-row" v-for="entry of content.displays">
-          <p>{{ entry.amount }}</p>
+      <div class="flex flex-col -mb-5">
+        <div class="flex flex-row gap-4 -mt-10 text-[80pt] italic font-bold" v-for="entry of content.displays">
+          <p class="text-purple">{{ entry.amount }}</p>
           <p>{{ entry.title }}</p>
         </div>
       </div>
