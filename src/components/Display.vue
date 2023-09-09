ba5b03d6cfc1562a8130d7f8e52e8baa606d4bf9
Filip Krunic <fkrunic9@gmail.com>
2023-09-08 19:03:07 -0700

    fixed margin issue on mobile

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -12,13 +12,15 @@ defineProps<{
     flex-row 
     gap-2
 
-    text-6xl
+    text-[40pt]
+    leading-[45pt]
     italic    
     font-bold
 
     desktop:gap-4
     desktop:-mt-10
     desktop:text-[80pt]
+    desktop:leading-[80pt]
     ">
     <p class="text-purple">{{ amount }}</p>
     <p class="">{{ title }}</p>
--- src/components/Mobile.vue
+++ src/components/Mobile.vue
@@ -6,44 +6,45 @@ import SubmitButton from './SubmitButton.vue';
 </script>
 
 <template>
+  <div class="flex flex-col items-center justify-center min-h-screen m-4">
 
-  <!-- Card -->
-  <div class="
-    flex 
-    flex-col 
-    items-center 
-    justify-start 
-    gap-16
-    p-6
+    <!-- Card -->
+    <div class="
+      flex 
+      flex-col 
+      items-center 
+      justify-start 
+      gap-16
 
-    mx-4
-    mt-20
+      w-full
+      p-6      
 
-    rounded-t-3xl
-    rounded-bl-3xl
-    rounded-br-[100px]
-    shadow-lg
+      rounded-t-3xl
+      rounded-bl-3xl
+      rounded-br-[100px]
+      shadow-lg
 
-    bg-white    
-    ">
+      bg-white    
+      ">
 
-    <!-- Date Entry -->
-    <div class="flex flex-row justify-between w-full">
-      <Entry :title="'DAY'" :placeholder="'DD'"></Entry>
-      <Entry :title="'MONTH'" :placeholder="'MM'"></Entry>
-      <Entry :title="'YEAR'" :placeholder="'YYYY'"></Entry>
-    </div>
+      <!-- Date Entry -->
+      <div class="flex flex-row justify-between w-full">
+        <Entry :title="'DAY'" :placeholder="'DD'"></Entry>
+        <Entry :title="'MONTH'" :placeholder="'MM'"></Entry>
+        <Entry :title="'YEAR'" :placeholder="'YYYY'"></Entry>
+      </div>
 
-    <!-- Divider -->
-    <div class="relative w-full border-solid border-[1px] border-light-gray">
-      <SubmitButton></SubmitButton>
-    </div>
+      <!-- Divider -->
+      <div class="relative w-full border-solid border-[1px] border-light-gray">
+        <SubmitButton></SubmitButton>
+      </div>
 
-    <!-- Display -->
-    <div class="flex flex-col items-start pb-8">
-      <Display :title="'years'" :amount="content.displays.years"></Display>
-      <Display :title="'months'" :amount="content.displays.months"></Display>
-      <Display :title="'days'" :amount="content.displays.days"></Display>
+      <!-- Display -->
+      <div class="flex flex-col items-start pb-8">
+        <Display :title="'years'" :amount="content.displays.years"></Display>
+        <Display :title="'months'" :amount="content.displays.months"></Display>
+        <Display :title="'days'" :amount="content.displays.days"></Display>
+      </div>
     </div>
   </div>
 </template>
